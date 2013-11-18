f1b6c0a5227395a4a8d08dbde190bea14f918b69
Mikhail Troshev <mishanga@yandex-team.ru>
2013-11-18 23:36:33 +0400

    input theme

--- common.blocks/input/_has-label/input_has-label.deps.js
+++
@@ -1,7 +0,0 @@
-[{
-    shouldDeps : { elem : 'label' }
-},
-{
-    tech : 'test.js',
-    mustDeps : { tech : 'bemhtml', block : 'input', elem : 'label' }
-}]
--- common.blocks/input/input.examples/simple.bemjson.js
+++ common.blocks/input/input.examples/simple.bemjson.js
@@ -1,63 +1,68 @@
 ({
     block : 'page',
+    mods : { theme : 'example' },
     head : [
         { elem : 'css', url : '_simple.css', ie : false },
         { elem : 'js', url : '_simple.js' }
     ],
-    content : [
+    content : { tag : 'table', content : [
         {
             block : 'input',
             id : 'id0',
-            mods : { 'disabled' : true },
+            mods : { size : 's', 'disabled' : true },
             name : 'name0',
             val : 'value0'
         },
         {
             block : 'input',
+            mods : { size : 'm', 'has-clear' : true },
             id : 'id1',
             name : 'name1',
             val : 'value1'
         },
         {
             block : 'input',
-            mods : { 'has-clear' : true },
+            mods : { size : 'l', 'has-clear' : true },
             id : 'id2',
             name : 'name2',
-            val : 'value2'
+            val : 'value2',
+            placeholder : 'hint2'
         },
         {
             block : 'input',
-            mods : { 'has-hint' : true },
+            mods : { size : 's' },
             id : 'id3',
             name : 'name3',
             val : '',
-            hint : 'hint3'
+            placeholder : 'hint3'
         },
         {
             block : 'input',
-            mods : { 'has-hint' : true, 'has-label' : true },
+            mods : { size : 'm', type : 'password' },
             id : 'id4',
             name : 'name4',
-            val : '',
-            hint : 'hint4',
-            label : 'label4'
+            val : 'password',
+            placeholder : 'password'
         },
         {
             block : 'input',
-            mods : { 'has-hint' : true, 'has-label' : true },
+            mods : { size : 'l', type : 'textarea' },
             id : 'id5',
             name : 'name5',
-            val : 'value5',
-            hint : 'hint5',
-            label : 'label5',
-            maxLength : 42,
-            tabIndex : 42,
-            autocomplete : false
+            val : 'long long long long long text'
         }
     ].map(function(input) {
         return {
-            tag : 'p',
-            content : input
+            tag : 'tr',
+            content : ['', 'simple', 'normal'].map(function(theme) {
+                var c = JSON.parse(JSON.stringify(input));
+                c.mods || (c.mods = {});
+                c.mods.theme = theme;
+                return {
+                    tag : 'td',
+                    content : c
+                };
+            })
         };
-    })
+    }) }
 });
